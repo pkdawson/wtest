f66263925d41c0b3bc8b23c3e1e8d2799d745a19
Patrick Dawson <pat@dw.is>
2016-08-30 04:49:45 +0200

    stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 int main()
 {
+    auto dur = 200ms;
     cout << "hello, world\n";
+    cout << dur.count() << "ms\n";
     return 0;
 }
